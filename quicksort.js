98b289aa20d2f370e9264c937c1be2706b328d27
Madison May <madison.may@students.olin.edu>
2013-06-22 23:21:07 +0200

    JS implementation of quicksort

---
+++ quicksort.js
@@ -0,0 +1,26 @@
+function quicksort(array) {
+    if (array.length <= 1) {
+        return array
+    }
+    var pivot_index = Math.floor((Math.random()*array.length));
+    var pivot = array[pivot_index];
+    array.splice(pivot_index, 1);
+    var less = [];
+    var greater = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] <= pivot) {
+            less.push(array[i]);
+        } else {
+            greater.push(array[i]);
+        }
+    }
+    return quicksort(less).concat([pivot]).concat(quicksort(greater));
+}
+
+//test cases
+array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+console.log('Unsorted list: ', array);
+array = quicksort(array)
+console.log('Sorted list: ', array);
+
+
